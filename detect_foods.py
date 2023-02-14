97b413c532e8d581990c0631ae7aa75cf34c28b4
Matt Morgan <mattmorgan6@gmail.com>
2023-02-14 00:10:12 -0800

    Fix archive image location error

--- detect_foods.py
+++ detect_foods.py
@@ -82,7 +82,7 @@ async def run_waste_detection(remote_url, initDistance = 0):
         
         # Makes the name of the file and the location for capture and displaying.
         image_name = 'waste_{}.jpg'.format(curr_datetime_str)
-        image_location = f'archive_check_detection_images/pre_detection_{image_name}.jpg'
+        image_location = f'archive_detection_images/pre_detection_{image_name}.jpg'
         
         # Annotates the text with distance, time, weight and captures the image.
         # camera.annotate_text = "Captured: {} \n Weight: {:0.2f} Distance: {:0.2f}".format(curr_datetime_str, weightgram, dist) 
@@ -128,6 +128,7 @@ calibrateButton = init_sensors.setup_button(board, digitalio)
 
 
 async def main():
+    print('Starting food detector!')
 
     if not api_calls.check_for_secrets():
         return

28ee758831ae0199628961a7e7caaf2b1b19d9bf
theusaf <theusafyt@gmail.com>
2023-07-31 16:04:49 -0700

    feat: update readme and install scripts

--- README.md
+++ README.md
@@ -4,17 +4,12 @@
 
 SSH or open up a terminal on the Jetson Nano and run the following commands:
 
-1. `cd /home/osu/Desktop`
-2. `mkdir auto-detect`
-3. `cd auto-detect`
-4. `wget https://raw.githubusercontent.com/Soundbendor/food-detection-embedded/main/food_detector.sh`
-5. `chmod +x food_detector.sh`
-6. `./food_detector.sh install`
-7. add the .env file with the secrets to current directory (`auto-detect/`)
-8. `crontab -e`
-  - the program will ask you which text editor to use, select nano or your preferred editor.
-  - then, add the following as the last line: <br>
-`@reboot /bin/sleep 15 && cd /home/osu/Desktop/auto-detect/ && ./auto-detect/food_detector.sh pull+detect >> ./auto-detect/logs/auto_detect_log_$(date).txt 2>&1`
+```bash
+cd /home/osu/Desktop
+git clone https://github.com/Soundbendor/food-detection-embedded.git
+cd food-detection-embedded
+./food_detector.sh install_prod
+```
 
 <br>
 
@@ -35,6 +30,14 @@ Note: do not unplug the Jetson Nano while the detection script is running, this 
 python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
+# To link the built-in opencv library to the virtual environment
+ln -s /usr/lib/python3.6/dist-packages/cv2/python-3.6/cv2*.so ./venv/lib/python3.6/site-packages/cv2.so
+```
+
+or
+
+```bash
+./food_detector.sh install
 ```
 
 ### How to run the detector:
--- food_detector.sh
+++ food_detector.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 command_arg=$1
+MAIN="./src/food_waste/main.py"
 
 function pull_changes {
     echo 'Pulling changes from GitHub...'
-    cd food-detection-embedded/
     git pull --ff-only
 
     echo 'Installing dependencies...'
@@ -16,25 +16,33 @@ function pull_changes {
 function run_detection {
     echo 'Running detections....'
     echo '(press ctrl+c to stop)'
-    sudo ./food-detection-embedded/venv/bin/python3 ./food-detection-embedded/detect_foods.py $2
+    sudo ./venv/bin/python3 $MAIN $2
 }
 
-if [ "${command_arg}" == "install" ]; then
-    git clone https://github.com/Soundbendor/food-detection-embedded.git
-
+function install {
     mkdir -p archive_detection_images
     mkdir -p archive_check_focus_images
     mkdir -p logs
 
     echo 'Creating virtual environment for dependencies...'
-    cd ./food-detection-embedded
     python3 -m venv venv
-    echo 'Installing dependencies (takes a minute)...'
+    echo 'Installing dependencies (may take a minute)...'
     ./venv/bin/pip3 install -qr requirements.txt
-    
+
+    echo 'Copying native opencv library to virtual environment...'
+    ln -s /usr/lib/python3.6/dist-packages/cv2/python-3.6/cv2*.so ./venv/lib/python3.6/site-packages/cv2.so
+
     cd ..
+}
+
+if [ "${command_arg}" == "install" ]; then
+    install
     echo ''
     echo 'Done. Now use `./food_detector.sh detect` to detect some foods!'
+
+elif [ "${command_arg}" == "install_prod" ]; then
+    install
+    # TODO: setup services
 
 elif [ "${command_arg}" == "pull" ]; then
     pull_changes
@@ -51,14 +59,16 @@ elif [ "${command_arg}" == "detect" ]; then
 
 elif [ "${command_arg}" == "focus" ]; then
     echo 'Checking camera focus...'
-    echo '(press ctrl+c to stop)'
-    sudo ./food-detection-embedded/venv/bin/python3 ./food-detection-embedded/detect_foods.py focus $2
+    sudo ./venv/bin/python3 $MAIN --dry $2
 
 elif [ "${command_arg}" == "clean" ]; then
-    echo 'This will delete all files and code in the repo (but keep the archive files). Are you sure you want to proceed? (y/n)'
+    echo 'This will delete all files and code in the repo (but copy archive files). Are you sure you want to proceed? (y/n)'
     read decision
     echo ''
     if [ "${decision}" == "yes" ] || [ "${decision}" == "y" ]; then
+        cd ..
+        cp -r ./food-detection-embedded/archive_detection_images ./archive_detection_images
+        cp -r ./food-detection-embedded/archive_check_focus_images ./archive_check_focus_images
         rm -rf ./food-detection-embedded
         echo 'All files removed.'
     else
@@ -66,5 +76,5 @@ elif [ "${command_arg}" == "clean" ]; then
     fi
 else
     echo '`${command_arg}` not recognized as a command.'
-    echo 'Please use: `detect`, `focus`, `pull`, `clean`, or `install`.'
+    echo 'Please use: `detect`, `focus`, `pull`, `pull+detect`, `clean`, `install_prod`, or `install`.'
 fi

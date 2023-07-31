17715f6ee9486986a805b00473b9828b7dad4735
theusaf <theusafyt@gmail.com>
2023-07-31 10:45:13 -0700

    feat: create gpio map

--- src/food_waste/io/pin.py
+++ src/food_waste/io/pin.py
@@ -1,3 +1,25 @@
 # This file maps named pins to GPIO numbers
 
-LIGHT = 12 # pin 37, GPIO12
+# LED
+LIGHT = 26 # pin 37
+
+# HTU21D-F
+TEMPERATURE_SDA = 3 # pin 5
+TEMPERATURE_SCL = 2 # pin 3
+
+# HX711
+WEIGHT_CLK = 27 # pin 13
+WEIGHT_DAT = 17 # pin 11
+
+# A1M8 UART (Lidar Sensor)
+LIDAR_MONOCTL = 23 # pin 16
+LIDAR_TXD = 15 # pin 10
+LIDAR_RXD = 14 # pin 8
+
+# Camera
+CAMERA_SCK = 20 # pin 38
+CAMERA_WS = 19 # pin 35
+CAMERA_SD = 18 # pin 12
+
+# Button/Switch
+BUTTON = 16 # pin 36

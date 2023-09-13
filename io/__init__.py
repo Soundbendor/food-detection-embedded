d617d6dc743ae840d365288727f1d02fd000edcf
theusaf <theusafyt@gmail.com>
2023-09-13 14:49:25 -0700

    refactor: remove higher-level classes from io
    - higher-level classes should be implemented per-branch.
    - the library should only contain the lower-level implementations

--- io/__init__.py
+++ io/__init__.py
@@ -1,6 +1,4 @@
-from . import pin as PIN
-from .status_light import StatusLight, StatusLightStatus
-from .tare_button import TareButton, TareButtonStatus
-from .weight_sensor import WeightSensor
-from .camera import Camera
-from .depth_camera import DepthCamera
+from .button import ButtonComponent, ButtonComponentEvents
+from .light import LightComponent
+from .weight_sensor import WeightSensorComponent, WeightSensorComponentEvents
+from .camera import CameraComponent
--- io/component/__init__.py
+++
@@ -1,4 +0,0 @@
-from .button import ButtonComponent, ButtonComponentEvents
-from .light import LightComponent
-from .weight_sensor import WeightSensorComponent, WeightSensorComponentEvents
-from .camera import CameraComponent

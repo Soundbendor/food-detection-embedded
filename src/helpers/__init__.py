5586cfed95f8f54b36605ea284dd999bea93c0b2
Aidan-B1409 <aidanb1049@gmail.com>
2024-04-26 05:16:03 -0700

    Update API request to add new segmentation files

--- src/helpers/__init__.py
+++ src/helpers/__init__.py
@@ -1,24 +1,26 @@
-import logging
-import sys
-import os
 import json
-from time import time
+import logging
+import os
+import sys
 import uuid
+from time import time
+
 import httpx
 
-
 TWO_HOURS_SECONDS = 7200
-#TWO_HOURS_SECONDS = 20
+# TWO_HOURS_SECONDS = 20
 
-class TimeHelper():
+
+class TimeHelper:
     def __init__(self):
         self.lastTime = time()
-    
+
     """
     Check to see if we have reached the two hour interval and should trigger the collection
 
     :return A bool representing if our 2 hour interval is up
     """
+
     def twoHourInterval(self) -> bool:
         currentTime = int(time())
         if (currentTime % TWO_HOURS_SECONDS) == 0 and currentTime != self.lastTime:
@@ -26,41 +28,54 @@ class TimeHelper():
             return True
         return False
 
+
 """
 General wrapper for initializing logging formats and file storage
 """
-class Logging():
+
+
+class Logging:
     """
     Configure logging format and output type based on arguments passed to the program
 
     :param path: The current file path of the module creating our logging module
     :param verbose: Determines wether or not we should be printing all of the info messages or just warning and higher
     """
+
     def __init__(self, verbose=True):
-        FORMAT = '%(asctime)s [%(filename)s:%(funcName)s:%(lineno)d] [%(levelname)s] %(message)s'
+        FORMAT = "%(asctime)s [%(filename)s:%(funcName)s:%(lineno)d] [%(levelname)s] %(message)s"
 
         loggingLevel = logging.INFO
         if not verbose:
             loggingLevel = logging.WARNING
 
         # Check if we want to specify an output file for the logging
-        if(len(sys.argv) < 2):
+        if len(sys.argv) < 2:
             logging.basicConfig(format=FORMAT, level=loggingLevel)
-            logging.info("No output file specified file logging will be disabled to enable: ./main.py <outputfilepath>")
+            logging.info(
+                "No output file specified file logging will be disabled to enable: ./main.py <outputfilepath>"
+            )
         else:
-            logging.basicConfig(format=FORMAT, level=logging.INFO, handlers=[logging.FileHandler(sys.argv[1]), logging.StreamHandler()])
+            logging.basicConfig(
+                format=FORMAT,
+                level=logging.INFO,
+                handlers=[logging.FileHandler(sys.argv[1]), logging.StreamHandler()],
+            )
+
 
 """
 Load sensor calibration details from a given file 
 """
-class CalibrationLoader():
 
+
+class CalibrationLoader:
     """
     Create a new instance of our calibration data loader
 
     :param file: The name of the JSON file our calibration data is stored in
     """
-    def __init__(self, file = "CalibrationDetails.json"):
+
+    def __init__(self, file="CalibrationDetails.json"):
         logging.info(f"Retrieving calibration details from file: {file}")
 
         # Attempt to open the file and convert the contents to JSON object
@@ -68,51 +83,73 @@ class CalibrationLoader():
             self.data = json.load(f)
             logging.info("Succsessfully loaded calibration details!")
 
-    
     """
     Retrieve the given calibration data
 
     :param field: The key name where the calibration data is stored
     """
+
     def get(self, field):
         return self.data[field]
-    
+
+
 """
 Handles requests to remote APIs (S3 and FastAPI)
 """
-class RequestHandler():
-    def __init__(self, dataDir = "../data", secret_file="config.secret"):
+
+
+class RequestHandler:
+    def __init__(self, dataDir="../data", secret_file="config.secret"):
         self.secret_file = secret_file
         self.dataDir = dataDir
         self.apiKey, self.endpoint, self.port = self.loadFastAPICredentials(secret_file)
         self.endpoint = f"http://{self.endpoint}:{self.port}"
-    
+
     """
     Upload the most recent files in the 'data' folder to our remote server for storage
 
     :return: Dictionary of base file names (ie. "colorImage") mapped to the stored location in our S3 bucket
     """
+
     def uploadFiles(self, fileNames: dict):
         endpoint = self.endpoint + "/api/upload_files"
 
         # Format request headers
-        headers = {
-            "token": self.apiKey
-        }
+        headers = {"token": self.apiKey}
 
         filesToUpload = {
-                "colorImageFile": ("colorImage.jpg", open(fileNames["colorImage"], "rb"), "image/jpg"),
-                "depthImageFile": ("depthImage.jpg", open(fileNames["depthImage"], "rb"), "image/jpg"),
-                "heatmapImageFile": ("heatmap.jpg", open(fileNames["heatmapImage"], "rb"), "image/jpg"),
-                "topologyMapFile": ("depth.ply", open(fileNames["topologyMap"], "rb"), "application/octet-stream"),
-                "voiceRecordingFile": ("downsampledAudio.wav", open(fileNames["voiceRecording"], "rb"), "audio/wav")
-            }
-
-        params = {
-            "deviceID": int(uuid.getnode())
+            "colorImageFile": (
+                "colorImage.jpg",
+                open(fileNames["colorImage"], "rb"),
+                "image/jpg",
+            ),
+            "depthImageFile": (
+                "depthImage.jpg",
+                open(fileNames["depthImage"], "rb"),
+                "image/jpg",
+            ),
+            "heatmapImageFile": (
+                "heatmap.jpg",
+                open(fileNames["heatmapImage"], "rb"),
+                "image/jpg",
+            ),
+            "topologyMapFile": (
+                "depth.ply",
+                open(fileNames["topologyMap"], "rb"),
+                "application/octet-stream",
+            ),
+            "voiceRecordingFile": (
+                "downsampledAudio.wav",
+                open(fileNames["voiceRecording"], "rb"),
+                "audio/wav",
+            ),
         }
-    
-        response = httpx.post(endpoint, params=params, headers=headers, files=filesToUpload).json()
+
+        params = {"deviceID": int(uuid.getnode())}
+
+        response = httpx.post(
+            endpoint, params=params, headers=headers, files=filesToUpload
+        ).json()
 
         if response["status"] == True:
             del response["status"]
@@ -122,41 +159,46 @@ class RequestHandler():
         else:
             logging.info(f"Failed to upload files to server!")
             return {}
-    
+
     """
     Test method to verify our API key is functioning
 
     :param apiKey: Our API Key used to authenticate with our API
     """
+
     def sendHeartbeat(self):
         endpoint = self.endpoint + "/api/health/heartbeat"
         response = httpx.get(endpoint).json()
-       
-        if 'is_alive' in response and response["is_alive"] == True:
+
+        if "is_alive" in response and response["is_alive"] == True:
             logging.info("Succsessfully recieved hearbeat!")
             return True
         else:
             logging.error("Failed to recieive heartbeat from server!")
             return False
-    
+
     """
     Request that the API credentials be updated to the most recent ones stored in the file
     """
+
     def updateAPICreds(self):
-        self.apiKey, self.endpoint, self.port = self.loadFastAPICredentials(self.secret_file)
+        self.apiKey, self.endpoint, self.port = self.loadFastAPICredentials(
+            self.secret_file
+        )
         self.endpoint = f"http://{self.endpoint}:{self.port}"
-    
+
     """
     Send a secure heartbeat request to the API
     """
+
     def sendSecureHeartbeat(self):
         endpoint = self.endpoint + "/api/health/secure_heartbeat"
         headers = {
             "token": self.apiKey,
         }
         response = httpx.get(endpoint, headers=headers).json()
-       
-        if 'is_alive' in response and response["is_alive"] == True:
+
+        if "is_alive" in response and response["is_alive"] == True:
             logging.info("Succsessfully recieved hearbeat!")
             return True
         else:
@@ -168,12 +210,13 @@ class RequestHandler():
 
     :param data: The complete JSON data packet 
     """
+
     def sendAPIRequest(self, fileNames: dict, data: dict):
         endpoint = self.endpoint + "/api/scan"
         fileLocations = self.uploadFiles(fileNames)
 
         if len(fileLocations) > 0:
-        
+
             # API Request
             headers = {
                 "token": self.apiKey,
@@ -184,6 +227,8 @@ class RequestHandler():
                 "depthImage": str(fileLocations["depthImage"]),
                 "heatmapImage": str(fileLocations["heatmapImage"]),
                 "topologyMap": str(fileLocations["topologyMap"]),
+                "segmentImage": str(fileLocations["segmentImage"]),
+                "segmentResults": str(fileLocations["segmentResults"]),
                 "voiceRecording": str(fileLocations["voiceRecording"]),
                 "total_weight": float(data["NAU7802"]["data"]["weight"]),
                 "weight_delta": float(data["NAU7802"]["data"]["weight_delta"]),
@@ -193,13 +238,15 @@ class RequestHandler():
                 "iaq": float(data["BME688"]["data"]["iaq"]),
                 "co2_eq": float(data["BME688"]["data"]["CO2-eq"]),
                 "tvoc": float(data["BME688"]["data"]["bVOC-eq"]),
-                "transcription": str(data["SoundController"]["data"]["TranscribedText"]),
+                "transcription": str(
+                    data["SoundController"]["data"]["TranscribedText"]
+                ),
                 "userTrigger": bool(data["DriverManager"]["data"]["userTrigger"]),
-                "deviceID": int(uuid.getnode())
+                "deviceID": int(uuid.getnode()),
             }
 
             response = httpx.post(endpoint, headers=headers, json=payload).json()
-            if('status' in response and response["status"] == True):
+            if "status" in response and response["status"] == True:
                 logging.info("Data successfully uploaded!")
                 return True
             else:
@@ -207,15 +254,20 @@ class RequestHandler():
                 return False
         else:
             logging.error("No file names supplied from file upload!")
-            return False  
-    
+            return False
+
     """
     Load and return our Fast API credentials
 
     :param file: The file our credentials are stored in
     """
+
     def loadFastAPICredentials(self, file):
-        secretFile = open(file, 'r')
+        secretFile = open(file, "r")
         credsJson = json.load(secretFile)
         secretFile.close()
-        return (credsJson["FASTAPI_CREDS"]["apiKey"], credsJson["FASTAPI_CREDS"]["endpoint"], credsJson["FASTAPI_CREDS"]["port"])
+        return (
+            credsJson["FASTAPI_CREDS"]["apiKey"],
+            credsJson["FASTAPI_CREDS"]["endpoint"],
+            credsJson["FASTAPI_CREDS"]["port"],
+        )

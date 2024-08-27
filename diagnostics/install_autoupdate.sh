4726da7064cf14e37e2ff5fa4328892117d43e3b
Aidan-B1409 <aidanb1049@gmail.com>
2024-08-26 20:46:21 -0700

    Fix autoupdate

--- diagnostics/install_autoupdate.sh
+++ diagnostics/install_autoupdate.sh
@@ -5,5 +5,5 @@ rm -f /etc/cron.daily/autoUpdate.sh
 rm -f /etc/cron.hourly/getLogs.sh
 
 # Cat to crontab
-echo "5 4 * * * bash /home/sbcompost/food-detection-embedded/diagnostics/autoUpdate.sh" >>/var/spool/cron/crontabs/root
-echo "0 * * * * bash /home/sbcompost/food-detection-embedded/diagnostics/getLogs.sh" >>/var/spool/cron/crontabs/root
+echo "5 4 * * * /home/sbcompost/food-detection-embedded/diagnostics/autoUpdate.sh" >>/var/spool/cron/crontabs/root
+echo "0 * * * * /home/sbcompost/food-detection-embedded/diagnostics/getLogs.sh" >>/var/spool/cron/crontabs/root

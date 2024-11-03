d9161a88a4e1ea70182d9e91a496a3702ea3aeaa
Aidan-B1409 <aidanb1049@gmail.com>
2024-11-03 15:00:06 -0800

    Update auto-update script to support HTTPS

--- diagnostics/autoUpdate.sh
+++ diagnostics/autoUpdate.sh
@@ -7,12 +7,12 @@ server='sb-binsight.dri.oregonstate.edu:30080'
 lastUpdateFile='./diagnostics/lastUpdate.txt'
 updateOccuredFile='./data/updated.txt'
 
-getAPIKey () {
-    apiKey=$(cat ~/food-detection-embedded/src/config.secret | python -c "import sys, json; print(json.load(sys.stdin)['FASTAPI_CREDS']['apiKey'])")
+getAPIKey() {
+  apiKey=$(cat ~/food-detection-embedded/src/config.secret | python -c "import sys, json; print(json.load(sys.stdin)['FASTAPI_CREDS']['apiKey'])")
 }
 
-checkAPIForUpdates () {
-    updateResponse=$(curl -X 'GET' "http://$server/api/check_for_updates" -H "accept: application/json" -H "token: $apiKey" | python -c "import sys, json; print(json.load(sys.stdin)['lastUpdateTime'])")
+checkAPIForUpdates() {
+  updateResponse=$(curl -k -X 'GET' "https://$server/api/check_for_updates" -H "accept: application/json" -H "token: $apiKey" | python -c "import sys, json; print(json.load(sys.stdin)['lastUpdateTime'])")
 }
 
 getAPIKey
@@ -21,7 +21,7 @@ cd $HOME/food-detection-embedded
 
 # If lastUpdated file doesn't exist we should write a 0 to it to start
 if [ ! -f "$lastUpdateFile" ]; then
-  echo "0" > "$lastUpdateFile"
+  echo "0" >"$lastUpdateFile"
 fi
 
 # Get the last updated value from the file
@@ -32,29 +32,28 @@ checkAPIForUpdates
 
 # if the time that the last device was updated is less than the last time an update was pushed we want to update
 if (($LAST_UPDATED < $updateResponse)); then
-    # Stop and remove the container if it is running
-    if [ "$( docker container inspect -f '{{.State.Running}}' $name )" = "true" ]; then
-        echo "Stopping running container"
-        docker stop $name
-        docker rm binsight-firmware
-    fi
+  # Stop and remove the container if it is running
+  if [ "$(docker container inspect -f '{{.State.Running}}' $name)" = "true" ]; then
+    echo "Stopping running container"
+    docker stop $name
+    docker rm binsight-firmware
+  fi
 
-    # Download the updates from Github
-    echo "Downloading updated firmware..."
-    git pull
+  # Download the updates from Github
+  echo "Downloading updated firmware..."
+  git pull
 
-    # Update the last updated time
-    echo "Rebuilding firmware..."
-    echo $updateResponse > $lastUpdateFile
+  # Update the last updated time
+  echo "Rebuilding firmware..."
+  echo $updateResponse >$lastUpdateFile
 
-    # Create temp file to signify to the proccess that is restarting that this is a reboot as a result of an update, and should not talk
-    touch $updateOccuredFile
+  # Create temp file to signify to the proccess that is restarting that this is a reboot as a result of an update, and should not talk
+  touch $updateOccuredFile
 
-    # Rebuild and restart the container
-    ./buildDocker.sh
-    ./runDocker.sh    
-    
-    
+  # Rebuild and restart the container
+  ./buildDocker.sh
+  ./runDocker.sh
+
 else
-    echo "Nothing to update"
+  echo "Nothing to update"
 fi

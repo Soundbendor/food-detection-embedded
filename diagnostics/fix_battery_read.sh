797aab2f462ecb106a1ebf1d368acbce872a3572
Aidan-B1409 <aidanb1049@gmail.com>
2025-06-24 21:59:56 -0700

    Fix type

--- diagnostics/fix_battery_read.sh
+++ diagnostics/fix_battery_read.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [! -f "fixlock.txt" ]; then
+if [ ! -f "fixlock.txt" ]; then
 	sudo sed -i "s/^ExecStart=\/usr\/libexec\/bluetooth\/bluetoothd/ExecStart=\/usr\/libexec\/bluetooth\/bluetoothd -P battery/" /usr/lib/systemd/system/bluetooth.service
 	touch fixlock.txt
 fi

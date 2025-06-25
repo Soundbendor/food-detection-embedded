fc3e5a66895a8bd5a8146befdcd4971ef6fb5742
Aidan-B1409 <aidanb1049@gmail.com>
2025-06-24 21:58:36 -0700

    Fix iOS bluetooth battery service issue

--- diagnostics/fix_battery_read.sh
+++ diagnostics/fix_battery_read.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-sudo sed -i "s/^ExecStart=\/usr\/libexec\/bluetooth\/bluetoothd/ExecStart=\/usr\/libexec\/bluetooth\/bluetoothd -P battery/" /etc/systemd/system/bluetooth.target.wants/bluetooth.service
+if [! -f "fixlock.txt" ]; then
+	sudo sed -i "s/^ExecStart=\/usr\/libexec\/bluetooth\/bluetoothd/ExecStart=\/usr\/libexec\/bluetooth\/bluetoothd -P battery/" /usr/lib/systemd/system/bluetooth.service
+	touch fixlock.txt
+fi
